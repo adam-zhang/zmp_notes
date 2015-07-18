7c67aaae46e64ebf36a8285405fea16ba79062fe
mingpingzhang <mingpingzhang@163.com>
2015-07-18 08:39:56 +0800

    add appMain.cpp to project

---
+++ code/application/appMain.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+    > File Name: appMain.cpp
+    > Author: Mingping (Adam) Zhang
+    > Mail: mingpingzhang@163.com 
+    > Created Time: 2015- 7月-18 08时36分48秒
+ ************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	cout << "Hello world!" << endl;
+	return 0;
+};
