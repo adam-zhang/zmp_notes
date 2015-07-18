ef0bed32c0f468c87923e3a3aff027c6af1b18b7
mingpingzhang <mingpingzhang@163.com>
2015-07-18 21:28:46 +0800

    finished BaseObject

--- code/libDatabase/BaseObject.cpp
+++ code/libDatabase/BaseObject.cpp
@@ -6,3 +6,19 @@
  ************************************************************************/
 
 #include "BaseObject.h"
+
+BaseObject::BaseObject(BaseObject* parent)
+	: QObject(parent)
+{
+}
+
+const QUuid& BaseObject::id()
+{
+	return id_;
+}
+
+void BaseObject::setId(const QUuid& id)
+{
+	id_ = id;
+}
+
