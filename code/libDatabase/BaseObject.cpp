6c9a869ea56ddeec18b6c69e4c60ac90c87b81bf
mingpingzhang <mingpingzhang@163.com>
2015-07-18 18:09:52 +0800

    added BaseObject

---
+++ code/libDatabase/BaseObject.cpp
@@ -0,0 +1,8 @@
+/*************************************************************************
+    > File Name: BaseObject.cpp
+    > Author: Mingping (Adam) Zhang
+    > Mail: mingpingzhang@163.com 
+    > Created Time: 2015- 7月-18 18时04分10秒
+ ************************************************************************/
+
+#include "BaseObject.h"
---
+++ code/libDatabase/BaseObject.h
@@ -0,0 +1,22 @@
+/*************************************************************************
+    > File Name: /home/zmp/Projects/GitProjects/zmp_notes/code/libDatabase/BaseObject.h
+    > Author: Mingping (Adam) Zhang
+    > Mail: mingpingzhang@163.com 
+    > Created Time: 2015- 7月-18 18时04分41秒
+ ************************************************************************/
+
+#include <QObject>
+#include <QUuid>
+
+class BaseObject : public QObject
+{
+public:
+	BaseObject(BaseObject* obj = 0);
+private:
+	QUuid id_;
+
+public:
+	const QUuid& id();
+	void setId(const QUuid& id);
+
+};
