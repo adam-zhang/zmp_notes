e9c38c7ead32be268933f20a1943c155fd7f3407
mingpingzhang <mingpingzhang@163.com>
2015-07-20 08:19:02 +0800

    basic structure finished

---
+++ code/libUI/MainWindow.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+    > File Name: MainWindow.cpp
+    > Author: Mingping (Adam) Zhang
+    > Mail: mingpingzhang@163.com 
+    > Created Time: 2015- 7月-20 07时25分09秒
+ ************************************************************************/
+
+#include "MainWindow.h"
+
+MainWindow::MainWindow(QWidget*  parent)
+	: QMainWindow(parent)
+{
+
+}
---
+++ code/libUI/MainWindow.h
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: /home/zmp/Projects/GitProjects/zmp_notes/code/libUI/MainWindow.h
+    > Author: Mingping (Adam) Zhang
+    > Mail: mingpingzhang@163.com 
+    > Created Time: 2015- 7月-20 07时25分12秒
+ ************************************************************************/
+
+#include <QMainWindow>
+
+class MainWindow : public QMainWindow
+{
+	Q_OBJECT
+public:
+	explicit MainWindow(QWidget* parent = 0);
+};
